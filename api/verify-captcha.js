fa86cbac6f68acb6b9ea0839ba89190cf09b6273
azardeensd <140503898+azardeensd@users.noreply.github.com>
2025-11-05 11:05:10 +0530

    Update verify-captcha.js

--- api/verify-captcha.js
+++ api/verify-captcha.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     }
 
     // Verify with Google reCAPTCHA or hCaptcha (replace SITE_SECRET with your actual key)
-    const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+    const secretKey = "6LfrPAAsAAAAAOwUphq0Le1tnPrYok4Iwi98evyO";
 
     const response = await fetch(
       `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`,
